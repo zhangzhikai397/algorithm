dff3aba99b6c98404b5728060584815e633d879e
zhangzhikai397 <397354957@qq.com>
2019-12-01 22:39:54 +0800

    单链表定义

---
+++ src/main/java/com/huawei/linkedlist/SingleLinkedList.java
@@ -0,0 +1,30 @@
+package com.huawei.linkedlist;
+
+/**
+ * 单链表
+ */
+public class SingleLinkedList {
+    // 链表的大小
+    private int size;
+    // 链表的头节点
+    private Node head;
+
+    private class Node {
+        // 节点的数据域
+        private Object data;
+        // 指向下一个节点的指针
+        private Node next;
+        public Node(Object obj) {
+            this.data = obj;
+            this.next = null;
+        }
+    }
+
+    /**
+     * 单链表构造函数
+     */
+    public SingleLinkedList() {
+        size = 0;
+        head = null;
+    }
+}
